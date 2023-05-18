8497d0e29a278584674c88fe8f13a7715fee4429
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2023-05-18 16:21:40 -0300

    18/5%2023

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,10 +1,10 @@
 import logging
 import requests  
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
-# crea un log de los fallos y excepciones
+# Crea un log de los fallos y excepciones
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -12,26 +12,26 @@ logging.basicConfig(
 
 url = "http://api.weatherapi.com/v1/forecast.json?key=894dd5dcbf7d410da13234020230704&q=q=Lomas de Zamora&days=2&aqi=no&alerts=no"
 
-response = requests.get(url) # hacemos el llamado a la api 
+response = requests.get(url) # Realizamos el llamado a la api 
 
-data = response.json() # transformamos el llamado al formato json
+data = response.json() # Parseamos la data recibida a formato JSON
 
-grados_c_ahora = data["current"]["temp_c"] # accedemos a la temperatura actual
+grados_c_ahora = data["current"]["temp_c"] # Accedemos a la temperatura actual
 
-grados_c_ahora = str(grados_c_ahora) # casteamos para poder concatenar
+grados_c_ahora = str(grados_c_ahora) # Casteamos para poder concatenar
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Buenas!! La temperatura actualmente es de " + grados_c_ahora)
 
 
-# crea el bot
+# Crea el bot
 application = ApplicationBuilder().token('6124422376:AAGvV95nIq_JUyf_3mFVRPjiSa6Hyz8Td7w').build()
     
-# escucha el comando /start en el chat
+# Escucha el comando /start en el chat
 start_handler = CommandHandler('start', start) 
 application.add_handler(start_handler)
 
-# corre el codigo hasta que hagas control c
+# Corre el codigo hasta que hagas control c
 application.run_polling()
 
 
